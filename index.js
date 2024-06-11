14b254432fc55b4369044df25cd93da58d3a6585
armajk2 <145760299+armajk2@users.noreply.github.com>
2024-06-12 01:02:50 +0900

    test

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ function showPopup() {
     document.getElementById('popup').style.display = 'block';
 }
 
+function showPopup1() {
+    document.getElementById('popup1').style.display = 'block';
+}
+
+function showPopup2() {
+    document.getElementById('popup2').style.display = 'block';
+}
+
 function closePopup() {
     document.getElementById('popup').style.display = 'none';
 }
+
+function closePopup1() {
+    document.getElementById('popup1').style.display = 'none';
+}
+
+function closePopup2() {
+    document.getElementById('popup2').style.display = 'none';
+}
+
