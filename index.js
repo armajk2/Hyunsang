899b889dc3ce02dd31fa1d2eb0d7c52eb97ba5ce
armajk2 <145760299+armajk2@users.noreply.github.com>
2024-06-19 05:41:12 +0900

    az

--- index.js
+++ index.js
@@ -23,3 +23,38 @@ function closePopup2() {
     document.getElementById('popup2').style.display = 'none';
 }
 
+function showInfo1() {
+    // info1 요소 보이기
+    document.getElementById('info1').style.display = 'block';
+    // 나머지 info 요소 숨기기
+    document.getElementById('info2').style.display = 'none';
+    document.getElementById('info3').style.display = 'none';
+    document.getElementById('info4').style.display = 'none';
+}
+
+function showInfo2() {
+    // info2 요소 보이기
+    document.getElementById('info2').style.display = 'block';
+    // 나머지 info 요소 숨기기
+    document.getElementById('info1').style.display = 'none';
+    document.getElementById('info3').style.display = 'none';
+    document.getElementById('info4').style.display = 'none';
+}
+
+function showInfo3() {
+    // info3 요소 보이기
+    document.getElementById('info3').style.display = 'block';
+    // 나머지 info 요소 숨기기
+    document.getElementById('info1').style.display = 'none';
+    document.getElementById('info2').style.display = 'none';
+    document.getElementById('info4').style.display = 'none';
+}
+
+function showInfo4() {
+    // info4 요소 보이기
+    document.getElementById('info4').style.display = 'block';
+    // 나머지 info 요소 숨기기
+    document.getElementById('info1').style.display = 'none';
+    document.getElementById('info2').style.display = 'none';
+    document.getElementById('info3').style.display = 'none';
+}
